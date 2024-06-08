17b7b800f7f91e2f68572a308343a929eee451c7
abssharma <abhidel2002@gmail.com>
2024-06-07 23:22:46 -0700

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <header className = "header">
-            <NavLink to = "/" className = "w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-                <p className = "blue-gradient_text">ABS</p>
+            <NavLink to = "/" className = "w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md p-9">
+                <p className = "blue-gradient_text">ARCHI TEXTURE</p>
             </NavLink>
             <nav className = "flex text-lg gap-7 font-medium">
                 <NavLink to = "/about" className = {({ isActive }) => isActive ? 
